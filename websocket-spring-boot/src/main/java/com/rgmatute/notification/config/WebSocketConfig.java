4ac174f6a1a174db72709aec1afab0944fc00de2
Ronny Matute <rmatute@eclipdata.com>
2021-07-05 00:51:30 -0500

    add download server ws

--- websocket-spring-boot/src/main/java/com/rgmatute/notification/config/WebSocketConfig.java
+++ websocket-spring-boot/src/main/java/com/rgmatute/notification/config/WebSocketConfig.java
@@ -12,12 +12,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry registry) {
-		registry.setApplicationDestinationPrefixes("/app");
-		registry.enableSimpleBroker("/topic");
+		registry.setApplicationDestinationPrefixes("/app", "");
+		registry.enableSimpleBroker("/topic", "");
 	}
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/websocket").setAllowedOrigins("*");
+		registry.addEndpoint("/websocket", "").setAllowedOrigins("*");
 	}
 }
